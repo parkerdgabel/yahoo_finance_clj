1b7e72114c83036191dbbe030e956f8d8d3e11c5
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2021-12-10 13:08:08 -0700

    Updated descripts

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject yahoo_finance_clj "0.1.0-SNAPSHOT"
+(defproject yahoo_finance_clj "0.1.0"
   :description "Wrapper for yahoo finance api."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
