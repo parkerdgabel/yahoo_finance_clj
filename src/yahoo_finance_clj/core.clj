08474760377dc1e92c96f6e3610c44358283c9b8
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2021-12-10 09:18:53 -0700

    Added quoteSummary

--- src/yahoo_finance_clj/core.clj
+++ src/yahoo_finance_clj/core.clj
@@ -9,12 +9,16 @@
 
 (def ^:private base-url "https://yfapi.net")
 
+(defn- yahoo-get [url opts]
+  (json/read-str (:body (client/get url opts))))
+
 (def ^:private quote-url (str base-url "/v6/finance/quote"))
 
 (defn quote [parameters]
   (let [headers {:x-api-key @api-key}
-        params (assoc parameters :symbols (s/join "," (:symbols parameters)))]
-    (json/read-str (:body (client/get quote-url {:query-params params :headers headers})))))
+        params (assoc parameters :symbols (s/join "," (:symbols parameters)))
+        opts {:query-params params :headers headers}]
+    (yahoo-get quote-url opts)))
 
 (def ^:private options-url (str base-url "/v7/finance/options/"))
 
@@ -23,11 +27,24 @@
         url (str options-url sym)
         opts (if date  {:query-params {:date date} :headers headers}
                        {:headers headers})]
-    (json/read-str (:body (client/get url opts)))))
+    (yahoo-get url opts)))
 
 (def ^:private spark-url (str base-url "/v8/finance/spark"))
 
 (defn spark [parameters]
   (let [headers {:x-api-key @api-key}
-        params (assoc parameters :symbols (s/join "," (:symbols parameters)))]
-    (json/read-str (:body (client/get spark-url {:query-params params :headers headers})))))
+        params (assoc parameters :symbols (s/join "," (:symbols parameters)))
+        opts {:query-params params :headers headers}]
+    (yahoo-get spark-url opts)))
+
+(def ^:private quoteSummary-url (str base-url "/v11/finance/quoteSummary/"))
+
+(defn quote-summary [sym & parameters]
+  (let [headers {:x-api-key @api-key}
+        url (str quoteSummary-url sym)
+        modules (if (contains? parameters :modules)
+                  (s/join "," (:modules parameters))
+                  (s/join "," ["defaultKeyStatistics" "assetProfile"]))
+        params (assoc parameters :modules modules)
+        opts {:query-params params :headers headers}]
+    (yahoo-get url opts)))
