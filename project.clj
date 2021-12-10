5557979f532835f10fda18e583f6898af864397b
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2021-12-09 19:23:03 -0700

    call

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [clj-http "3.12.3"]]
+                 [clj-http "3.12.3"]
+                 [org.clojure/data.json "2.4.0"]]
   :repl-options {:init-ns yahoo-finance-clj.core})
--- src/yahoo_finance_clj/core.clj
+++ src/yahoo_finance_clj/core.clj
@@ -1,6 +1,22 @@
 (ns yahoo-finance-clj.core
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.string :as s]
+            [clojure.data.json :as json]))
+
+(defonce ^:private api-key (atom ""))
+
+(defn set-api-key [key] (reset! api-key key))
 
 (def ^:private base-url "https://yfapi.net")
+
+(def ^:private quote-url (str base-url "/v6/finance/quote"))
+
+(defn quote [parameters]
+  (let [headers {:x-api-key @api-key}
+        params (assoc parameters :symbols (s/join "," (:symbols parameters)))]
+    (json/read-str (client/get quote-url {:query-params params :headers headers}))))
+
+
+
 
 
