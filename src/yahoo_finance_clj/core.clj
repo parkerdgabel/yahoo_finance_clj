7166989a4bb9f364225c2ed890d87fcbfdf858db
Gabel, Parker D <parkerdgabel@email.arizona.edu>
2021-12-10 10:02:20 -0700

    Added chart

--- src/yahoo_finance_clj/core.clj
+++ src/yahoo_finance_clj/core.clj
@@ -48,3 +48,18 @@
         params (assoc parameters :modules modules)
         opts {:query-params params :headers headers}]
     (yahoo-get url opts)))
+
+(def ^:private chart-url (str base-url "/v8/finance/chart/"))
+
+(defn chart [ticker & parameters]
+  (let [headers {:x-api-key @api-key}
+        url (str chart-url ticker)
+        events (if (contains? parameters :events)
+                 (s/join "," (:events parameters))
+                 (s/join "," ["div" "split"]))
+        comparisons (if (contains? parameters :comparisons)
+                      (s/join "," (:comparisons parameters))
+                      "")
+        params (assoc parameters :events events :comparisons comparisons)
+        opts {:query-params params :headers headers}]
+    (yahoo-get url opts)))
